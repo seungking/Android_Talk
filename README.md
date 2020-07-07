dcef573a16d3d732cbb6df7acf3bbb2f9c37b36c
Seungki An <55059074+seungking@users.noreply.github.com>
2020-07-08 06:17:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Android_Talk
+ Firebase를 활용한 메신져 어플리케이션 서비스
---
+++ app/src/main/java/com/e/androidtalk/models/User.java
@@ -0,0 +1,56 @@
+package com.e.androidtalk.models;
+
+public class User {
+
+    private String uid, email, name, profileUrl;
+    private boolean selection;
+
+    public User(){
+
+    }
+
+    public User(String uid, String email, String name, String profileUrl) {
+        this.uid = uid;
+        this.email = email;
+        this.name = name;
+        this.profileUrl = profileUrl;
+    }
+
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public void setSelection(boolean selection) {
+        this.selection = selection;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
+
